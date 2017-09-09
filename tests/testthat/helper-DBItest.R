57e55e1b0d89b261ff1f6fad725b7a4dc87249a2
Kirill Müller <krlmlr+r@mailbox.org>
2017-09-09 21:58:14 +0200

    add requirement

--- tests/testthat/helper-DBItest.R
+++ tests/testthat/helper-DBItest.R
@@ -4,6 +4,7 @@ DBItest::make_context(
   tweaks = DBItest::tweaks(
     constructor_relax_args = TRUE,
     placeholder_pattern = "?",
-    logical_return = function(x) as.integer(x)
+    logical_return = function(x) as.integer(x),
+    list_temporary_tables = FALSE
   ),
   name = "RMariaDB")
