63dda0dc976ae584688dd5c05738984a9945f368
Kirill Müller <krlmlr+r@mailbox.org>
2017-09-24 14:02:37 +0200

    Revert "run downloader in configure.win"
    This reverts commit 8b3c09ad9abeaf02fb75bf6d80cf1dc2d405b8cb.

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,8 +1,8 @@
 # Link against libmariadbclient static libraries
-if(!file.exists("windows/libmariadbclient-2.3.3/include/mariadb/mysql.h")) {
+if(!file.exists("../windows/libmariadbclient-2.3.3/include/mariadb/mysql.h")){
   if(getRversion() < "3.3.0") setInternet2()
   download.file("https://github.com/rwinlib/libmariadbclient/archive/v2.3.3.zip", "lib.zip", quiet = TRUE)
-  dir.create("windows", showWarnings = FALSE)
-  unzip("lib.zip", exdir = "windows")
+  dir.create("../windows", showWarnings = FALSE)
+  unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
 }
