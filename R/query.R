08d471ab22d47d13ef5a860f31bb3d3914bdf352
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2017-10-18 10:05:43 +0200

    pull dbSendStatement() in from DBI
    In order to control the stmnt flag without inspecting the call stack
    from dbSendQuery(), dbSendStatement() is being added to RMariaDB.

--- R/query.R
+++ R/query.R
@@ -7,7 +7,10 @@ NULL
 #' To retrieve results a chunk at a time, use [dbSendQuery()],
 #' [dbFetch()], then [dbClearResult()]. Alternatively, if you want all the
 #' results (and they'll fit in memory) use [dbGetQuery()] which sends,
-#' fetches and clears for you.
+#' fetches and clears for you. For data manipulation queries (i.e. queries
+#' that do not return data, such as \code{UPDATE}, \code{DELETE}, etc.),
+#' [dbSendStatement()] serves as a counterpart to [dbSendQuery()], while
+#' [dbExecute()] corresponds to [dbGetQuery()].
 #'
 #' @param conn an [MariaDBConnection-class] object.
 #' @param res A  [MariaDBResult-class] object.
@@ -53,22 +56,33 @@ setMethod("dbFetch", "MariaDBResult",
 #' @export
 setMethod("dbSendQuery", c("MariaDBConnection", "character"),
   function(conn, statement, params = NULL, ...) {
-    statement <- enc2utf8(statement)
-
-    is_stmnt <- any(grepl("dbSendStatement", sys.calls()))
-
-    rs <- new("MariaDBResult",
-      sql = statement,
-      ptr = result_create(conn@ptr, statement, is_stmnt)
-    )
-
-    if (!is.null(params)) {
-      dbBind(rs, params)
-    }
-
-    rs
+    dbSend(conn, statement, params, FALSE)
   }
 )
+
+#' @rdname query
+#' @export
+setMethod("dbSendStatement", signature("MariaDBConnection", "character"),
+  function(conn, statement, ...) {
+    dbSend(conn, statement, NULL, TRUE)
+  }
+)
+
+dbSend <- function(conn, statement, params = NULL, is.stmnt = FALSE) {
+  statement <- enc2utf8(statement)
+
+  rs <- new("MariaDBResult",
+    sql = statement,
+    ptr = result_create(conn@ptr, statement, is.stmnt)
+  )
+
+  if (!is.null(params)) {
+    dbBind(rs, params)
+  }
+
+  rs
+}
+
 
 #' @rdname query
 #' @export
