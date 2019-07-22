3e0063d41a46d6f7ce72f379786ca9c06725559d
Kirill Müller <krlmlr+r@mailbox.org>
2019-07-22 08:43:15 +0200

    Include hms and blob

--- tic.R
+++ tic.R
@@ -2,7 +2,7 @@ do_package_checks(args = strsplit(Sys.getenv("R_CHECK_ARGS"), " ")[[1]])
 
 if (ci_has_env("DEV_VERSIONS")) {
   get_stage("install") %>%
-    add_step(step_install_github(c("r-dbi/DBI", "r-dbi/DBItest")))
+    add_step(step_install_github(c("r-dbi/DBI", "r-dbi/DBItest", "tidyverse/hms", "tidyverse/blob")))
 }
 
 if (ci_has_env("BUILD_PKGDOWN") && !ci_is_tag()) {
