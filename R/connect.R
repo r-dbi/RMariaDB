0e9a0944991f60e3f45a3ba3857e5a3393a442d0
Philipp Schauberger <philipp@schauberger.co.at>
2020-12-02 20:43:12 +0100

    Update R/connect.R
    Co-authored-by: Kirill Müller <krlmlr@users.noreply.github.com>

--- R/connect.R
+++ R/connect.R
@@ -77,7 +77,7 @@ setMethod("dbConnect", "MariaDBDriver",
     groups = "rs-dbi", default.file = NULL, ssl.key = NULL, ssl.cert = NULL,
     ssl.ca = NULL, ssl.capath = NULL, ssl.cipher = NULL, ...,
     bigint = c("integer64", "integer", "numeric", "character"),
-    timeout = 10, timezone = "+0000") {
+    timeout = 10, timezone = "+00:00") {
 
     bigint <- match.arg(bigint)
 
