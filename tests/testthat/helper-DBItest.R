4ceef91fd1e80ea9c92da6ca2ef9dc2e0924fabd
Kirill Müller <krlmlr+r@mailbox.org>
2021-09-20 07:01:38 +0200

    Update for DBItest 1.7.2

--- tests/testthat/helper-DBItest.R
+++ tests/testthat/helper-DBItest.R
@@ -2,6 +2,7 @@ DBItest::make_context(
   MariaDB(),
   list(dbname = "test"),
   tweaks = DBItest::tweaks(
+    dbitest_version = "1.7.2",
     constructor_relax_args = TRUE,
     placeholder_pattern = "?",
     logical_return = function(x) as.integer(x),
@@ -12,11 +13,6 @@ DBItest::make_context(
     # result
     "data_logical",                               # not an error: cannot cast to logical
     "data_raw",                                   # not an error: can't cast to blob type
-
-    # bad tests
-    "list_objects_features",
-    "append_roundtrip_timestamp",
-    "roundtrip_timestamp",
 
     NULL
   )
