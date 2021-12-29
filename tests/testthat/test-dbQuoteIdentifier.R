3f05e55cfe4aaf5aa7654a05411e056e8d516b9d
Daniel Possenriede <possenriede@gmail.com>
2021-12-29 20:53:09 +0100

    Disconnect on exit in `dbQuoteIdentifier()` tests

--- tests/testthat/test-dbQuoteIdentifier.R
+++ tests/testthat/test-dbQuoteIdentifier.R
@@ -1,65 +1,66 @@
 test_that("quoting string", {
   con <- mariadbDefault()
+  on.exit(dbDisconnect(con))
 
   quoted <- dbQuoteIdentifier(con, "Robert'); DROP TABLE Students;--")
   expect_s4_class(quoted, 'SQL')
   expect_equal(as.character(quoted),
                "`Robert'); DROP TABLE Students;--`")
-  dbDisconnect(con)
 })
 
 test_that("quoting SQL", {
   con <- mariadbDefault()
+  on.exit(dbDisconnect(con))
 
   quoted <- dbQuoteIdentifier(con, SQL("Robert'); DROP TABLE Students;--"))
   expect_s4_class(quoted, 'SQL')
   expect_equal(as.character(quoted),
                "Robert'); DROP TABLE Students;--")
-  dbDisconnect(con)
 })
 
 test_that("quoting Id", {
   con <- mariadbDefault()
+  on.exit(dbDisconnect(con))
 
   quoted <- dbQuoteIdentifier(con, Id(schema = 'Robert', table = 'Students;--'))
   expect_s4_class(quoted, 'SQL')
   expect_equal(as.character(quoted),
                "`Robert`.`Students;--`")
-  dbDisconnect(con)
 })
 
 test_that("quoting Id with column, #254", {
   con <- mariadbDefault()
+  on.exit(dbDisconnect(con))
 
   quoted <- dbQuoteIdentifier(con, Id(schema = 'Robert', table = 'Students;--', column = "dormitory"))
   expect_s4_class(quoted, 'SQL')
   expect_equal(as.character(quoted),
                "`Robert`.`Students;--`.`dormitory`")
-  dbDisconnect(con)
 })
 
 test_that("quoting Id with column, unordered", {
   con <- mariadbDefault()
+  on.exit(dbDisconnect(con))
 
   quoted <- dbQuoteIdentifier(con, Id(column = "dormitory", table = 'Students;--'))
   expect_s4_class(quoted, 'SQL')
   expect_equal(as.character(quoted),
                "`Students;--`.`dormitory`")
-  dbDisconnect(con)
 })
 
 test_that("quoting errors", {
   con <- mariadbDefault()
+  on.exit(dbDisconnect(con))
 
   expect_error(dbQuoteIdentifier(con, Id(tabel = 'Robert')),
                "components")
   expect_error(dbQuoteIdentifier(con, Id(table = 'Robert', table = 'Students;--')),
                "Duplicated")
-  dbDisconnect(con)
 })
 
 test_that("unquoting identifier - SQL with quotes", {
   con <- mariadbDefault()
+  on.exit(dbDisconnect(con))
 
   expect_equal(dbUnquoteIdentifier(con, SQL('`Students;--`')),
                list(Id(table = 'Students;--')))
@@ -75,11 +76,11 @@ test_that("unquoting identifier - SQL with quotes", {
 
   expect_error(dbUnquoteIdentifier(con, SQL('`Robert.`Students`')),
                "^Can't unquote")
-  dbDisconnect(con)
 })
 
 test_that("unquoting identifier - SQL without quotes", {
   con <- mariadbDefault()
+  on.exit(dbDisconnect(con))
 
   expect_equal(dbUnquoteIdentifier(con, SQL('Students')),
                list(Id(table = 'Students')))
@@ -89,14 +90,13 @@ test_that("unquoting identifier - SQL without quotes", {
 
   expect_error(dbUnquoteIdentifier(con, SQL('Rob``ert.Students')),
                "^Can't unquote")
-  dbDisconnect(con)
 })
 
 test_that("unquoting identifier - Id", {
   con <- mariadbDefault()
+  on.exit(dbDisconnect(con))
 
   expect_equal(dbUnquoteIdentifier(con,
                                    Id(schema = 'Robert', table = 'Students;--')),
                list(Id(schema = 'Robert', table = 'Students;--')))
-  dbDisconnect(con)
 })
