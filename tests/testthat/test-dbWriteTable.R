d9793123e5907c83131612d057937c7be0265efc
Kun Ren <renkun@outlook.com>
2022-03-01 13:27:35 +0800

    Add test case

--- tests/testthat/test-dbWriteTable.R
+++ tests/testthat/test-dbWriteTable.R
@@ -32,6 +32,14 @@ test_that("dbAppendTable() throws error if constraint violated", {
   expect_error(dbAppendTable(con, "t1", x), "added 7 rows|Duplicate entry")
 })
 
+test_that("dbAppendTable() works with Id", {
+  con <- mariadbDefault()
+  on.exit(dbDisconnect(con))
+
+  dbExecute(con, "create table T(n integer primary key)")
+  expect_equal(dbAppendTable(con, Id(table = "T"), data.frame(n = 1:10)), 10)
+})
+
 # Available only in MariaDB
 test_that("can read file from disk", {
   con <- mariadbDefault()
