70afd5667b55c59f18e93267656e86bd2da8b1d6
Kirill Müller <krlmlr+r@mailbox.org>
2022-10-20 04:04:06 +0200

    Fix cpp11::cpp_register()

--- src/result.cpp
+++ src/result.cpp
@@ -3,7 +3,7 @@
 #include "MariaResult.h"
 
 
-// [[cpp11::register]]
+[[cpp11::register]]
 cpp11::external_pointer<DbResult>
 result_create(cpp11::external_pointer<DbConnectionPtr> con, std::string sql, bool is_statement = false) {
   (*con)->check_connection();
@@ -12,43 +12,43 @@ result_create(cpp11::external_pointer<DbConnectionPtr> con, std::string sql, boo
 }
 
 // TODO: figure out what is release in RSQLite
-// [[cpp11::register]]
+[[cpp11::register]]
 void result_release(cpp11::external_pointer<DbResult> res) {
   res.release();
 }
 
-// [[cpp11::register]]
+[[cpp11::register]]
 bool result_valid(cpp11::external_pointer<DbResult> res_) {
   DbResult* res = res_.get();
   return res != NULL && res->is_active();
 }
 
-// [[cpp11::register]]
+[[cpp11::register]]
 cpp11::list result_fetch(DbResult* res, const int n) {
   return (SEXP)res->fetch(n);
 }
 
-// [[cpp11::register]]
+[[cpp11::register]]
 void result_bind(DbResult* res, cpp11::list params) {
   res->bind((SEXP)params);
 }
 
-// [[cpp11::register]]
+[[cpp11::register]]
 bool result_has_completed(DbResult* res) {
   return res->complete();
 }
 
-// [[cpp11::register]]
+[[cpp11::register]]
 int result_rows_fetched(DbResult* res) {
   return res->n_rows_fetched();
 }
 
-// [[cpp11::register]]
+[[cpp11::register]]
 int result_rows_affected(DbResult* res) {
   return res->n_rows_affected();
 }
 
-// [[cpp11::register]]
+[[cpp11::register]]
 cpp11::list result_column_info(DbResult* res) {
   return (SEXP)res->get_column_info();
 }
