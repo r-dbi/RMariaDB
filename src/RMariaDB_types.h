0c6c1ae3f157964de7a76795ed83731f5712b79b
Kirill Müller <kirill@cynkra.com>
2022-10-20 10:24:54 +0000

    fix build

--- src/RMariaDB_types.h
+++ src/RMariaDB_types.h
@@ -17,4 +17,24 @@ DbResult* as(SEXP x);
 
 }
 
+namespace cpp11 {
+
+template <typename T>
+enable_if_t<std::is_same<decay_t<T>, DbConnection*>::value, decay_t<T>> as_cpp(SEXP from) {
+  DbConnectionPtr* connection = (DbConnectionPtr*)(R_ExternalPtrAddr(from));
+  if (!connection)
+    stop("Invalid connection");
+  return connection->get();
+}
+
+template <typename T>
+enable_if_t<std::is_same<decay_t<T>, DbResult*>::value, decay_t<T>> as_cpp(SEXP from) {
+  DbResult* result = (DbResult*)(R_ExternalPtrAddr(from));
+  if (!result)
+    stop("Invalid result set");
+  return result;
+}
+
+}
+
 #endif
