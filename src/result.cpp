09a242f019e93ca4bf72b761e6c2dea960fdf58e
Kirill Müller <kirill@cynkra.com>
2022-10-21 20:25:28 +0000

    more proper way to clear pointer

--- src/result.cpp
+++ src/result.cpp
@@ -14,7 +14,7 @@ result_create(cpp11::external_pointer<DbConnectionPtr> con, std::string sql, boo
 // TODO: figure out what is release in RSQLite
 [[cpp11::register]]
 void result_release(cpp11::external_pointer<DbResult> res) {
-  delete res.release();
+  res.reset();
 }
 
 [[cpp11::register]]
