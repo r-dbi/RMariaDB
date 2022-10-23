79cef4fe4d8140d0fd44790147076703eb64a5f4
Kirill Müller <kirill@cynkra.com>
2022-10-23 12:24:12 +0000

    change interface

--- src/MariaResultImpl.h
+++ src/MariaResultImpl.h
@@ -10,7 +10,7 @@ public:
   virtual void send_query(const std::string& sql) = 0;
   virtual void close() = 0;
 
-  virtual void bind(const List& params) = 0;
+  virtual void bind(const cpp11::list& params) = 0;
 
   virtual cpp11::writable::list get_column_info() = 0;
 
