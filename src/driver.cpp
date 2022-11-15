01866a72b8ff7c1b1d04c354bda2c42e881493b9
Antonov Misha <antonov5551998@gmail.com>
2022-11-15 12:00:35 +0000

    remove case indent

--- src/driver.cpp
+++ src/driver.cpp
@@ -10,22 +10,22 @@
 
 // [[Rcpp::export]]
 void driver_init() {
-	mysql_library_init(0, NULL, NULL);
+  mysql_library_init(0, NULL, NULL);
 }
 
 // [[Rcpp::export]]
 void driver_done() {
-	mysql_library_end();
+  mysql_library_end();
 }
 
 // [[Rcpp::export]]
 IntegerVector version() {
-	return IntegerVector::create(
-	    _[SERVER_VERSION] = MYSQL_VERSION_ID,
-	    _[mysql_get_client_info()] = mysql_get_client_version());
+  return IntegerVector::create(
+      _[SERVER_VERSION] = MYSQL_VERSION_ID,
+      _[mysql_get_client_info()] = mysql_get_client_version());
 }
 
 // [[Rcpp::export]]
 void init_logging(const std::string& log_level) {
-	plog::init_r(log_level);
+  plog::init_r(log_level);
 }
