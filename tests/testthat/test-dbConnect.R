cc70833cf2072ab27786749e782a4e0f40ecdc64
Kirill Müller <kirill@cynkra.com>
2023-10-06 16:39:02 +0200

    Add test

---
+++ tests/testthat/test-dbConnect.R
@@ -0,0 +1,15 @@
+test_that("Connecting with mysql = FALSE", {
+  con <- mariadbDefault()
+  on.exit(dbDisconnect(con))
+
+  expect_s4_class(con, "MariaDBConnection")
+  expect_false(is(con, "MySQLConnection"))
+})
+
+test_that("Connecting with mysql = TRUE", {
+  con <- mysqlDefault()
+  on.exit(dbDisconnect(con))
+
+  expect_s4_class(con, "MariaDBConnection")
+  expect_s4_class(con, "MySQLConnection")
+})
