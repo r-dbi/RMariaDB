9a10f9e717a39cb862b4c1d29da63253db82ad98
Kirill Müller <kirill@cynkra.com>
2024-06-27 09:26:58 +0200

    ci: Prepare for dynamic check matrix

---
+++ .github/versions-matrix.R
@@ -0,0 +1,9 @@
+data.frame(
+  os = c("macos-latest", "macos-latest", "ubuntu-22.04", "ubuntu-22.04", "ubuntu-22.04", "ubuntu-22.04"),
+  r = "release",
+  mysql_client = c("false", "true", "true", "true", "false", "false"),
+  RMARIADB_FORCE_MARIADBCONFIG = c(1, NA, NA, NA, NA, NA),
+  RMARIADB_FORCE_MYSQLCONFIG = c(NA, 1, NA, NA, NA, NA),
+  mysql_server = c(NA, NA, "true", "false", "true", "false"),
+  desc = c("mariadb_config", "mysql_config", "MySQL server", "MariaDB server, MySQL client", "MySQL server, MariaDB client", "MariaDB server + client")
+)
