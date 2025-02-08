02e68aa10c7779b35a940837ca6f1767f708720d
Kirill Müller <krlmlr+r@mailbox.org>
2025-02-08 15:48:42 +0100

    ci: Add old Windows

--- .github/versions-matrix.R
+++ .github/versions-matrix.R
@@ -1,4 +1,4 @@
-data.frame(
+macos_combos <- data.frame(
   os = c("macos-latest", "macos-latest", "ubuntu-22.04", "ubuntu-22.04", "ubuntu-22.04", "ubuntu-22.04"),
   r = "release",
   mysql_client = c("false", "true", "true", "true", "false", "false"),
@@ -7,3 +7,5 @@ data.frame(
   mysql_server = c(NA, NA, "true", "false", "true", "false"),
   desc = c("mariadb_config", "mysql_config", "MySQL server", "MariaDB server, MySQL client", "MySQL server, MariaDB client", "MariaDB server + client")
 )
+windows_versions <- data.frame(os = "windows-latest", r = r_versions[4:5])
+rbind(macos_combos, windows_versions)
