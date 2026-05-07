103ba32ab84d2dd0cf733b7fbb9459c0a158bf67
Phil Tran <tran@dc-energy.com>
2026-05-07 11:09:31 -0400

    feat(c++): route INT UNSIGNED to integer64 via variable_type_from_field_type

--- src/MariaResultPrep.cpp
+++ src/MariaResultPrep.cpp
@@ -249,8 +249,13 @@ void MariaResultPrep::cache_metadata() {
 
     bool binary = fields[i].charsetnr == 63;
     bool length1 = fields[i].length == 1;
-    MariaFieldType type =
-      variable_type_from_field_type(fields[i].type, binary, length1);
+    bool is_unsigned = (fields[i].flags & UNSIGNED_FLAG) != 0;
+    MariaFieldType type = variable_type_from_field_type(
+      fields[i].type,
+      binary,
+      length1,
+      is_unsigned
+    );
     types_.push_back(type);
   }
 }
