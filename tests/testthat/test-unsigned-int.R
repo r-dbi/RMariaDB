324045aa62000c143bcd5327a9aa35f93b587721
Phil Tran <tran@dc-energy.com>
2026-05-07 11:17:38 -0400

    test: add failing test for unsigned_int = 'numeric'

--- tests/testthat/test-unsigned-int.R
+++ tests/testthat/test-unsigned-int.R
@@ -14,3 +14,24 @@ test_that("unsigned_int defaults to integer64 and preserves values above 2^31", 
     c(NA, "0", "1", "2147483647", "2147483648", "4294967295")
   )
 })
+
+test_that("unsigned_int = 'numeric' returns doubles", {
+  skip_if_not(mariadbHasDefault())
+  con <- dbConnect(
+    MariaDB(),
+    default.file = "~/.my.cnf",
+    group = "rs-dbi",
+    dbname = "test",
+    unsigned_int = "numeric"
+  )
+  on.exit(dbDisconnect(con))
+
+  dbExecute(con, "CREATE TEMPORARY TABLE t_uint_num (x INT UNSIGNED)")
+  dbExecute(con, "INSERT INTO t_uint_num VALUES (0), (2147483648), (4294967295)")
+
+  out <- dbGetQuery(con, "SELECT x FROM t_uint_num ORDER BY x")
+
+  expect_type(out$x, "double")
+  expect_false(inherits(out$x, "integer64"))
+  expect_equal(out$x, c(0, 2147483648, 4294967295))
+})
